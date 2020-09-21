9a92fd8d80ec44fd33e452d754b96e959ee98c2c
Manuel Uberti <manuel.uberti@inventati.org>
2020-09-21 15:37:17 +0200

    Fix spelling

--- flymake-proselint.el
+++ flymake-proselint.el
@@ -22,7 +22,7 @@
 
 ;;; Commentary:
 
-;; This package add support for proselint (http://proselint.com/) in Flymake.
+;; This package adds support for proselint (http://proselint.com/) in Flymake.
 
 ;; Once installed, the backend can be enabled with:
 ;; (add-hook 'markdown-mode-hook #'flymake-proselint-setup)
