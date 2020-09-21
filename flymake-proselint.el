c3d57a4ae82b70df08d07f7a677914022a2d8226
Manuel Uberti <manuel.uberti@inventati.org>
2020-09-21 15:20:18 +0200

    Add keyword and license

--- flymake-proselint.el
+++ flymake-proselint.el
@@ -4,8 +4,21 @@
 ;;
 ;; Author: Manuel Uberti <manuel.uberti@inventati.org>
 ;; Version: 1.0.0
+;; Keywords: convenience
 ;; Package-Requires: ((emacs "26.1") (flymake-quickdef "1.0.0"))
 ;; URL: https://github.com/manuel-uberti/flymake-proselint
+
+;; flymake-proselint is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 3, or (at your option) any later version.
+;;
+;; flymake-proselint is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with flymake-proselint.  If not, see http://www.gnu.org/licenses.
 
 ;;; Commentary:
 
